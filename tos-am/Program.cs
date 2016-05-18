5e4595242e39d6cb22a32a1580fe19a404dcc85c
LeoLombardi <leonardobotinelly+github@gmail.com>
2016-05-17 20:07:54 -0400

    Scaffolding

--- tos-am/Program.cs
+++ tos-am/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace tos_am
+namespace com.lombardi.tos.am
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace tos_am
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
